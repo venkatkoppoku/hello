94a65bb991bf528407ecfb132b6f877965b1c546
sam smith <sam@example.com>
2016-11-14 10:20:02 +0530

    initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,19 @@
+
+object Main extends App {
+
+
+  import java.util.Calendar
+
+  def isFridayThirteen(cal: Calendar): Boolean = {
+    val dayOfWeek = cal.get(Calendar.DAY_OF_WEEK)
+    val dayOfMonth = cal.get(Calendar.DAY_OF_MONTH)
+    // Scala returns the result of the last expression in a method
+    (dayOfWeek == Calendar.FRIDAY) && (dayOfMonth == 10)
+  }
+
+  while (!isFridayThirteen(Calendar.getInstance())) {
+    println("Today isn't Friday the 13th. Lame.")
+    // sleep for a day
+    Thread.sleep(86400000)
+  }
+}
---
+++ src/main/scala/WorkSheet.sc
@@ -0,0 +1,12 @@
+import java.util.Calendar
+def isFridayThirteen(cal: Calendar): Boolean = {
+  val dayOfWeek = cal.get(Calendar.DAY_OF_WEEK)
+  val dayOfMonth = cal.get(Calendar.DAY_OF_MONTH)
+  // Scala returns the result of the last expression in a method
+  (dayOfWeek == Calendar.FRIDAY) && (dayOfMonth == 11)
+}
+while (!isFridayThirteen(Calendar.getInstance())) {
+  println("Today isn't Friday the 13th. Lame.")
+  // sleep for a day
+  Thread.sleep(86400000)
+}
